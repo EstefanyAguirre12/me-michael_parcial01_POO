636ce213d44b1cf462bf5c95fd75fe73d97c2d34
Andresss21 <estefanyaguirre1200@hotmail.com>
2020-04-18 11:34:18 -0600

    add: Empresa and ServicioProfesional class

---
+++ Parcial1/src/com/parcial/Empresa.java
@@ -0,0 +1,30 @@
+package com.parcial;
+
+import java.util.List;
+
+public class Empresa {
+
+    private String nombre;
+    List<Empleado> planilla;
+
+    public void Empresa(String name){
+        this.nombre =nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public List<Empleado> getPlanilla() {
+        return planilla;
+    }
+
+    public void addEmpleado(String Empleado){
+
+    }
+
+    public void quitEmpelado(String Empleado){
+
+    }
+}
+
